8fd8974528890c1ea1341d1c94fbe81fa4a8880a
arsyanueva <162649694+arsyanueva@users.noreply.github.com>
2025-05-21 09:54:05 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Coba;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 //import java.io.*;
 
 /*
@@ -87,3 +89,15 @@ Route::get('danantara', function () {
 Route::get('frontend', function () {
     return view('frontend');
 });
+
+Route::get('dosen', [Coba::class, 'index']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('/formulir', [PegawaiController::class, 'formulir']); //halaman isian formulir
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']); // action form
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
